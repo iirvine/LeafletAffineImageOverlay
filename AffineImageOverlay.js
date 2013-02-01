566962f154e161d407a6f843ba63d46506df31f7
iirvine <icirvine@gmail.com>
2013-02-01 10:33:51 -0800

    -git init -bump leaflet version -stub out Leaflet-ized AffineOverlay class

---
+++ AffineImageOverlay.js
@@ -0,0 +1,22 @@
+L.AffineImageOverlay = L.Class.extend({
+	options: {
+		icon: L.Icon.Default,
+		boundingScale: .75
+	},
+
+	initialize: function(url, options) {
+
+	},
+
+	onAdd: function(map) {
+
+	},
+
+	onRemove: function(map) {
+
+	},
+
+	addTo: function(map) {
+
+	},
+});
